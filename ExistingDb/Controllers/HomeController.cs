6b45acbd319d569c17fcecb8d40262f0b79e041d
murat <muratdemir.pau@gmail.com>
2019-02-11 21:10:11 +0300

    11.02.2019_21:09

---
+++ ExistingDb/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExistingDb.Models.Scaffold;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExistingDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ScaffoldContext _context;
+        public HomeController(ScaffoldContext context) => _context = context;
+        public IActionResult Index()
+        {
+            return View(_context.Shoes
+                .Include(s => s.Color)
+                .Include(s => s.SalesCampaigns)
+                .Include(s => s.ShoeCategoryJunction)
+                    .ThenInclude(j => j.Category)
+                .Include(s => s.Fitting));
+        }
+    }
+}
---
+++ ExistingDb/Models/Logic/Shoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExistingDb.Models.Scaffold
+{
+    public partial class Shoes
+    {
+        public decimal PriceIncTax => Price * 1.2m;
+    }
+}
