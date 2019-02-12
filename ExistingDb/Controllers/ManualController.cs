1a0ad9fa2d697b50943580526bd09c17862b20cc
murat <muratdemir.pau@gmail.com>
2019-02-13 01:02:56 +0300

    13.02.2019_0102

---
+++ ExistingDb/Controllers/ManualController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExistingDb.Models.Manual;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExistingDb.Controllers
+{
+    public class ManualController : Controller
+    {
+        private readonly ManualContext _context;
+        public ManualController(ManualContext context) =>
+            _context = context;
+        public IActionResult Index()
+        {
+            ViewBag.Styles = _context.ShoeStyles.Include(s => s.Products);
+            ViewBag.Widths = _context.ShoeWidths.Include(s => s.Products);
+            ViewBag.Categories = _context.Categories
+            .Include(c => c.Shoes).ThenInclude(j => j.Shoe);
+            return View(_context.Shoes.Include(s => s.Style)
+            .Include(s => s.Width).Include(s => s.Categories)
+            .ThenInclude(j => j.Category));
+        }
+        public IActionResult Edit(long id)
+        {
+            ViewBag.Styles = _context.ShoeStyles;
+            ViewBag.Widths = _context.ShoeWidths;
+            ViewBag.Categories = _context.Categories;
+            return View(_context.Shoes.Include(s => s.Style)
+            .Include(s => s.Campaign)
+            .Include(s => s.Width).Include(s => s.Categories)
+            .ThenInclude(j => j.Category).First(s => s.Id == id));
+        }
+        [HttpPost]
+        public IActionResult Update(Shoe shoe, long[] newCategoryIds,
+ShoeCategoryJunction[] oldJunctions)
+        {
+            IEnumerable<ShoeCategoryJunction> unchangedJunctions
+            = oldJunctions.Where(j => newCategoryIds.Contains(j.CategoryId));
+            _context.Set<ShoeCategoryJunction>()
+            .RemoveRange(oldJunctions.Except(unchangedJunctions));
+            shoe.Categories = newCategoryIds.Except(unchangedJunctions
+            .Select(j => j.CategoryId))
+            .Select(id => new ShoeCategoryJunction
+            {
+                ShoeId = shoe.Id,
+                CategoryId = id
+            }).ToList();
+            _context.Shoes.Update(shoe);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- ExistingDb/Startup.cs
+++ ExistingDb/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExistingDb.Models.Manual;
 using ExistingDb.Models.Scaffold;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,7 +24,9 @@ namespace ExistingDb
             services.AddMvc();
             string conString = Configuration["ConnectionStrings:DefaultConnection"];
             services.AddDbContext<ScaffoldContext>(options =>
-            options.UseSqlServer(conString));
+                options.UseSqlServer(conString));
+            services.AddDbContext<ManualContext>(options =>
+                options.UseSqlServer(conString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
